8975b4d03d279637d15d9c597a7ae7dadb3d0ec2
Inaki Sebastian Gonzalo <igonzalo@crm.ucsc.edu>
2017-12-01 16:46:35 -0800

    romoved build and dist

--- TapeSensorTest.X/build/default/production/IO_Ports.o.d
+++
@@ -1,5 +0,0 @@
-build/default/production/IO_Ports.o:  \
-IO_Ports.c  \
- C:/CMPE118/include/BOARD.h  \
-C:/CMPE118/include/serial.h  \
- C:/CMPE118/include/IO_Ports.h 
--- TapeSensorTest.X/build/default/production/Stepper.o.d
+++
@@ -1,5 +0,0 @@
-build/default/production/Stepper.o:  \
-Stepper.c  \
-Stepper.h  \
- C:/CMPE118/include/BOARD.h  \
-C:/CMPE118/include/IO_Ports.h 
